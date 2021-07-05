b3c556af26632c663e4fde1ef2be5d94478db730
Sayalichobhe <86147765+Sayalichobhe@users.noreply.github.com>
2021-07-05 23:32:29 +0530

    Create area  function.js

---
+++ Full stack/Javascript/function/area  function.js
@@ -0,0 +1,17 @@
+
+ function area(){
+	 const pi=3.14;
+	 let r= 10;
+	 let l= 5;
+	 let b= 6;
+	 let s= 8;
+	 let ar=l*b;
+	 let ac= pi*r*r;
+	 let as=s*s;
+	 document.write("Area of circle is"+ ac) ; 
+	 document.write("Area of rectangle is"+ ar) ;
+	 document.write("Area of square is"+ as) ;
+	   }
+	
+    area();
+	
