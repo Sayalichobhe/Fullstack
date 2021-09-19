1c5f8eb63db0f6d2347a8ebce284419e1ca7425e
Sayalichobhe <86147765+Sayalichobhe@users.noreply.github.com>
2021-09-19 20:20:48 +0530

    Create function.py

---
+++ Python/function.py
@@ -0,0 +1,35 @@
+#Area of circle, rectangle and square.:
+'''
+def area():
+    pi=3.14
+    r=10
+    s=5
+    l=8
+    b=9
+    acr=pi*r*r
+    asqr=s*s
+    arect=l*b
+    print("Area of circle of radius",r,"is",acr,"meter square")
+    print("Area of square of side",s,"is",asqr,"meter square")
+    print("Area of rectangle of",l,"*",b,"is",arect,"meter square")
+area()  #function call
+'''
+
+
+#Factorial of given number.:
+ 
+def factorial():
+    n=3
+    i=1
+    f=1
+    while(i<=n):
+        f=f*i
+        i+1
+    print(f)
+
+factorial()
+ 
+     
+    
+    
+    
